05d071dd04e5904976d4c906961b88e7d84850f0
alekseik1 <1alekseik1@gmail.com>
2022-05-09 13:23:02 +0300

    add genie instructions

--- install-wsl.sh
+++ install-wsl.sh
@@ -63,3 +63,36 @@ if [ "`cat $PROGRESS_FILE`" -lt "5" ]; then
   echo 5 > "$PROGRESS_FILE"
 fi
 
+# if [ -z ${NO_GENIE+x} ]; then
+#     # install genie
+#     systemctl set-default multi-user.target
+#     sudo apt install lsb-release
+#     sudo wget -O /etc/apt/trusted.gpg.d/wsl-transdebian.gpg https://arkane-systems.github.io/wsl-transdebian/apt/wsl-transdebian.gpg
+
+#     sudo chmod a+r /etc/apt/trusted.gpg.d/wsl-transdebian.gpg
+
+#     sudo cat << EOF > /etc/apt/sources.list.d/wsl-transdebian.list
+# deb https://arkane-systems.github.io/wsl-transdebian/apt/ $(sudo lsb_release -cs) main
+# deb-src https://arkane-systems.github.io/wsl-transdebian/apt/ $(sudo lsb_release -cs) main
+# EOF
+
+#     sudo apt update
+#     sudo apt install genie
+#     # HACKS
+#     sudo e2label /dev/sdb cloudimg-rootfs
+#     sudo ssh-keygen -A
+#     sudo systemctl disable multipathd
+# cat << EOF > /etc/systemd/network/10-eth0.network
+# [Match]
+# Name=eth0
+
+# [Link]
+# Unmanaged=yes
+
+# [Network]
+# DHCP=no
+
+# [DHCP]
+# UseDNS=false
+# EOF
+# fi
